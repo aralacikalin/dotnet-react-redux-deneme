b49bc30ed89be412644c2d70f95cdab9f387c45f
Aral Acikalin <aralacikalin@hotmail.com>
2020-10-13 14:21:13 +0300

    dark theme biraz daha iyi çalışılması lazım

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -4,13 +4,20 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
+import {DarkTheme} from "./themes/DarkTheme.json";
+import {ThemeProvider} from "@fluentui/react-theme-provider";
 
 import './custom.css'
+import { createTheme } from '@fluentui/react';
 
-export default () => (
+export default () => {
+    const mytheme=createTheme({palette:DarkTheme});
+    return(
+        <ThemeProvider theme={mytheme}>
     <Layout>
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
     </Layout>
-);
+    </ThemeProvider>
+)};
--- ClientApp/src/components/Layout.tsx
+++ ClientApp/src/components/Layout.tsx
@@ -3,19 +3,19 @@ import { Container } from 'reactstrap';
 import NavMenu from './NavMenu';
 import {NavBar} from "./NavBar";
 import {ThemeProvider} from "@fluentui/react-theme-provider";
-import { createTheme } from '@fluentui/react';
+import { createTheme, loadTheme } from '@fluentui/react';
 import {DarkTheme} from "../themes/DarkTheme.json";
 
 export default (props: { children?: React.ReactNode }) => {
-    const mytheme=createTheme({palette:DarkTheme});
-    
+    loadTheme({palette:DarkTheme});
     return(
-        <React.Fragment>
-        <ThemeProvider theme={mytheme}>
+    <React.Fragment>
+
+
         <NavMenu/>
         <Container>
-                {props.children}
+            {props.children}
         </Container>
-    </ThemeProvider>
+
     </React.Fragment>
 )};
