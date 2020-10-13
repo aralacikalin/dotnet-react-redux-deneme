9dc3fa1adb70bcca42b9ad6e1ac3ac6754c1ad7b
Aral Acikalin <aralacikalin@hotmail.com>
2020-10-13 14:06:45 +0300

    tam çalışmıyor dark theme

--- ClientApp/src/components/Layout.tsx
+++ ClientApp/src/components/Layout.tsx
@@ -2,12 +2,20 @@ import * as React from 'react';
 import { Container } from 'reactstrap';
 import NavMenu from './NavMenu';
 import {NavBar} from "./NavBar";
+import {ThemeProvider} from "@fluentui/react-theme-provider";
+import { createTheme } from '@fluentui/react';
+import {DarkTheme} from "../themes/DarkTheme.json";
 
-export default (props: { children?: React.ReactNode }) => (
-    <React.Fragment>
+export default (props: { children?: React.ReactNode }) => {
+    const mytheme=createTheme({palette:DarkTheme});
+    
+    return(
+        <React.Fragment>
+        <ThemeProvider theme={mytheme}>
         <NavMenu/>
         <Container>
-            {props.children}
+                {props.children}
         </Container>
+    </ThemeProvider>
     </React.Fragment>
-);
+)};
