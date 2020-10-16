0413dcae96fa9de5d155d9f7cd66fcf0794ea74c
Aral Acikalin <aralacikalin@hotmail.com>
2020-10-16 13:16:44 +0300

    new add item component created

--- ClientApp/src/components/AdventureworksItem.tsx
+++ ClientApp/src/components/AdventureworksItem.tsx
@@ -1,4 +1,4 @@
-import { TextField } from '@fluentui/react';
+import { CompoundButton, Stack, TextField } from '@fluentui/react';
 import * as React from 'react';
 
 interface IState{
@@ -17,24 +17,38 @@ export default class AdventureworksItem extends React.PureComponent<{},IState>{
             groupName:"",
             date:""
         }
+        this.onNameChange=this.onNameChange.bind(this)
+        this.onGroupNameChange=this.onGroupNameChange.bind(this)
+        this.onDateChange=this.onDateChange.bind(this)
+        this.clicked=this.clicked.bind(this)
     }
-    onchange(event:React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string){
-        if(event.currentTarget.className=="date"){
-            this.setState({
-                date:newValue||""
-            })
-        }
+    onDateChange(event:React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string){
+        this.setState({
+            date:newValue||""
+        })
+    }
+    onNameChange(event:React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string){
+        this.setState({
+            name:newValue||""
+        })
+    }
+    onGroupNameChange(event:React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string){
+        this.setState({
+            groupName:newValue||""
+        })
     }
     clicked(){
-        alert(this.state.date)
+        console.log(this.state.date,this.state.name,this.state.groupName)
     }
     render(){
         return(
             <div>
-                <TextField label="Department Name" underlined required value={this.state.name}/>
-                <TextField label="Department Group Name" underlined required value={this.state.groupName}/>
-                <TextField className="date" label="Date" value={this.state.date} onChange={this.onchange} underlined required/>
-                <button onClick={this.clicked}></button>
+                <Stack>
+                    <TextField label="Department Name" value={this.state.name} onChange={this.onNameChange} underlined  />
+                    <TextField label="Department Group Name" value={this.state.groupName} onChange={this.onGroupNameChange} underlined  />
+                    <TextField label="Date" value={this.state.date} onChange={this.onDateChange} underlined />
+                </Stack>
+                <CompoundButton primary onClick={this.clicked}>Add Item</CompoundButton>
             </div>
         );
     }
