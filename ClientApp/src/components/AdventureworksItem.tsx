e2c5efd086b0fc92b477b0caa89ec2b94277f319
Aral Acikalin <aralacikalin@hotmail.com>
2020-10-16 15:36:25 +0300

    add menu is working

--- ClientApp/src/components/AdventureworksItem.tsx
+++ ClientApp/src/components/AdventureworksItem.tsx
@@ -1,4 +1,4 @@
-import { CompoundButton, Stack, TextField } from '@fluentui/react';
+import { CompoundButton, DatePicker, initializeIcons, MaskedTextField, Stack, TextField } from '@fluentui/react';
 import * as React from 'react';
 
 interface IState{
@@ -12,6 +12,7 @@ export default class AdventureworksItem extends React.PureComponent<{},IState>{
 
     constructor(props:any) {
         super(props);
+        initializeIcons()
         this.state={
             name:"",
             groupName:"",
@@ -38,15 +39,46 @@ export default class AdventureworksItem extends React.PureComponent<{},IState>{
         })
     }
     clicked(){
-        console.log(this.state.date,this.state.name,this.state.groupName)
+        if(this.state.name!==""&&this.state.groupName!==""){
+            var body={}
+
+            if(this.state.date==""){
+
+                body={Name:this.state.name,GroupName:this.state.groupName}
+            }
+            else{
+
+                body={Name:this.state.name,GroupName:this.state.groupName,ModifiedDate:this.state.date}
+            }
+
+            fetch("https://localhost:5001/adventureworks",
+            {method: 'POST',headers:{"Content-Type": "application/json"},
+                body:JSON.stringify(body)
+            })
+            .then((res)=>
+            {
+                console.log(res)
+                if(res.ok){
+                    alert("Department is added")
+                }
+                else{
+                    alert("Department is not added")
+    
+                }
+            })
+            console.log(this.state.date,this.state.name,this.state.groupName)
+        }
+        else{
+            alert("Enter all required fields")
+        }
     }
     render(){
         return(
             <div>
                 <Stack>
-                    <TextField label="Department Name" value={this.state.name} onChange={this.onNameChange} underlined  />
-                    <TextField label="Department Group Name" value={this.state.groupName} onChange={this.onGroupNameChange} underlined  />
-                    <TextField label="Date" value={this.state.date} onChange={this.onDateChange} underlined />
+                    <TextField label="Department Name" value={this.state.name} onChange={this.onNameChange} underlined required  />
+                    <TextField label="Department Group Name" value={this.state.groupName} onChange={this.onGroupNameChange} underlined required />
+                    <MaskedTextField label="Date" value={this.state.date} onChange={this.onDateChange} mask="9999-99-99" underlined maskChar="Y-M-D" />
                 </Stack>
                 <CompoundButton primary onClick={this.clicked}>Add Item</CompoundButton>
             </div>
